d24a3be1db9cdd55225d04abcd00b1b782fb5a97
Atnabon <atnabond@gmail.com>
2022-12-29 11:57:12 +0300

    updated

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import images from './routes/api/index';
+
+const app = express();
+const port = 3000;
+
+/**
+ * Get routes from the images routes and
+ * use them in our main application
+ **/
+app.use('/api', images);
+
+/**
+ * The following callback is executed for requests to ./ whether
+ * using GET, POST, PUT, DELETE, or any other HTTP request method
+ **/
+app.all('/', (req: express.Request, res: express.Response): void => {
+  res
+    .status(200)
+    .send(
+      'Welcome to the image processing api. See example api call http://localhost:3000/api/images?filename=palmtunnel&width=500&height=500'
+    );
+});
+
+app.listen(port, (): void => {
+  console.log(`server started at http://localhost:${port}`);
+});
+
+export default app;
